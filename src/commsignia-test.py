c6c1417ce180d4e627dc2a652d870cb44691b79d
Davide Pesavento <davide.pesavento@nist.gov>
2025-04-09 16:08:45 -0400

    Add testing tool for Commsignia devices

--- src/commsignia-test.py
+++ src/commsignia-test.py
@@ -1,16 +1,16 @@
-# Testing tool for Cohda MK6 OBU and RSU
+# Testing tool for Commsignia OBU and RSU
 
 import argparse
 import datetime
-import time
 from fabric import ThreadingGroup as Group
+from fabric.exceptions import GroupException
+from invoke.exceptions import CommandTimedOut
 
 
 # Devices under test (DUTs)
 hosts = {
-    "fe80::6e5:48ff:fe50:c78": "obu1",
-    "fe80::6e5:48ff:fe50:cb8": "obu2",
-    "fe80::6e5:48ff:fe30:710": "rsu1",
+    "192.168.0.74": "ob4",
+    "192.168.0.64": "rs4",
 }
 
 
@@ -31,14 +31,14 @@ def positive_int(arg):
     return value
 
 
-parser = argparse.ArgumentParser(description="Testing tool for Cohda MK6 OBU and RSU")
+parser = argparse.ArgumentParser(description="Testing tool for Commsignia OBU and RSU")
 parser.add_argument("-o", "--obu", action="extend", nargs="+", default=[], help="OBUs to test")
 parser.add_argument("-r", "--rsu", action="extend", nargs="+", default=[], help="RSUs to test")
 parser.add_argument("-n", "--repeat", type=positive_int, default=1,
                     help="how many times to repeat the test (default: %(default)s)")
 parser.add_argument("-t", "--duration", type=positive_int, default=60,
                     help="duration of each test repetition, in seconds (default: %(default)s)")
-parser.add_argument("-u", "--user", default="user",
+parser.add_argument("-u", "--user", default="root",
                     help="username for device login (default: %(default)s)")
 parser.add_argument("-c", "--no-copy", action="store_true",
                     help="skip copying the test results to the local machine")
@@ -60,51 +60,37 @@ if not obus and not rsus:
     parser.exit(message="No known devices selected, exiting.\n")
 all_devs = Group.from_connections(obus + rsus)
 
-results = all_devs.sudo("cv2x_get_status", in_stream=False)
+results = all_devs.run("commsignia-device-info", in_stream=False)
 print_exit_status(results)
-
-binary = "/mnt/rw/example1609/rc.example1609"
-files_to_copy = frozenset(
-    (
-        "cfg",
-        "conf.all",
-        "conf.diff",
-        "config.gz",
-        "gps.pcap",
-        "info",
-        "rx.pcap",
-        "rx_pc5.pcap",
-        "stderr",
-        "stdout",
-        "tx.pcap",
-        "tx_pc5.pcap",
-    )
-)
-timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+print()
+# muci always exits with status 1 for some reason
+# results = all_devs.run("muci its get capture", in_stream=False, warn=True)
+# print_exit_status(results)
+# print()
 
 for i in range(args.repeat):
-    print_progress(i, "Starting test")
-    results = obus.sudo(f"{binary} start obu", in_stream=False)
-    results |= rsus.sudo(f"{binary} start rsu", in_stream=False)
-    print_exit_status(results)
-
-    print_progress(i, f"Waiting {args.duration} seconds")
-    time.sleep(args.duration)
-
-    print_progress(i, "Stopping test")
-    results = rsus.sudo(f"{binary} stop rsu", in_stream=False)
-    results |= obus.sudo(f"{binary} stop obu", in_stream=False)
-    print_exit_status(results)
+    print_progress(i, f"Running test for {args.duration} seconds")
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"c2p_{timestamp}.pcap"
+    try:
+        results = all_devs.run(
+            f"tcpdump -pi lo -w /tmp/{filename} port 7943",
+            hide=True,
+            in_stream=False,
+            timeout=args.duration)
+    except GroupException as e:
+        for conn, res in e.result.items():
+            status = "OK" if isinstance(res, CommandTimedOut) else str(res)
+            print(f"*** {hosts[conn.host]} --> {status}")
 
     if args.no_copy:
         continue
 
     print_progress(i, "Transferring files")
-    for f in files_to_copy:
-        print(f"  {f}")
-        for conn in all_devs:
-            conn.get(
-                f"/tmp/log/current/{f}",
-                local=f"{args.directory}/{timestamp}/{i}/{hosts[conn.host]}/{f}",
-                preserve_mode=False,
-            )
+    print(f"  {filename}")
+    for conn in all_devs:
+        conn.get(
+            f"/tmp/{filename}",
+            local=f"{args.directory}/{hosts[conn.host]}_{timestamp}",
+            preserve_mode=False,
+        )
